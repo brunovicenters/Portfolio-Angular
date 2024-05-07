398a58db16155ae09a8f6d00c5da29eca9675be7
bruno <brunovicente.dev@gmail.com>
2024-05-07 00:05:02 -0300

    Added sobre mim + fixed app-navbar location

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { SobreMimComponent } from '../sobre-mim/sobre-mim.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, SobreMimComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
