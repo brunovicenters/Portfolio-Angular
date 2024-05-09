56b974a4523d991472a8e7475a3f24f47dac0c80
bruno <brunovicente.dev@gmail.com>
2024-05-09 18:11:00 -0300

    Added description

--- src/app/trajetoria/trajetoria.component.ts
+++ src/app/trajetoria/trajetoria.component.ts
@@ -14,10 +14,11 @@ export class TrajetoriaComponent {
   contagem: number = 0;
 
   trajetorias: Trajetoria[] = [
-    new Trajetoria(2021, 'Comecei o curso de Introdução à Programação de Computadores'),
-    new Trajetoria(2022, 'The web developer bootcamp'),
-    new Trajetoria(2023, 'Connect Code'),
-    new Trajetoria(2023, 'Tecnólogo em Sistemas para Internet')
+    new Trajetoria(2021, 'Introdução à Programação de Computadores', 'Comecei minha jornada no mundo da programação através do curso do Novotec Expresso, chamado de Introdução à programação de computadores, fornecido pela Etec de Taboão da Serra'),
+    new Trajetoria(2022, 'The web developer bootcamp', ''),
+    new Trajetoria(2023, 'Connect Code', ''),
+    new Trajetoria(2023, 'Tecnólogo em Sistemas para Internet' , ''),
+    new Trajetoria(2024, 'CIT Senac', '')
   ];
 
 }
--- src/model/trajetoriaModel.ts
+++ src/model/trajetoriaModel.ts
@@ -1,9 +1,11 @@
 export class Trajetoria {
   year: number;
   achivement: string;
+  description: string;
 
-  constructor(year: number, achivement: string) {
+  constructor(year: number, achivement: string, description: string) {
     this.year = year;
     this.achivement = achivement;
+    this.description = description;
   }
 }
