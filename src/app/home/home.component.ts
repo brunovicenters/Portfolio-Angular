06cc4e1413f47c4d59f9fc212f197baf69793447
bruno <brunovicente.dev@gmail.com>
2024-05-09 09:15:26 -0300

    Added trajetoria

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { SobreMimComponent } from '../sobre-mim/sobre-mim.component';
+import { TrajetoriaComponent } from '../trajetoria/trajetoria.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, SobreMimComponent],
+  imports: [NavbarComponent, SobreMimComponent, TrajetoriaComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
